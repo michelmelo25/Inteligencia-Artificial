ae12623aeaab99dc1ce18d2a4d7e46e849a7e058
michelmelo25 <michelmelosj25@hotmail.com>
2018-09-19 09:43:07 -0300

    controller update

--- RomaniaMap/src/map/Controller.java
+++ RomaniaMap/src/map/Controller.java
@@ -11,7 +11,6 @@ public class Controller {
 
     private ArrayList<State> states;
     private ArrayList<Action> actions;
-    private Map<String, Integer> h;
     private Model model;
     private BuscaEmLargura buscaEmLargura;
 
@@ -19,10 +18,8 @@ public class Controller {
         super();
         this.states = new ArrayList<>();
         this.actions = new ArrayList<>();
-        this.h = new HashMap<>();
         initializeStates();
         initializeActions();
-        initializeHeuristic();
         this.model = new Model(states, actions);
         this.buscaEmLargura = new BuscaEmLargura(states,actions);
     }
@@ -103,26 +100,4 @@ public class Controller {
 
     public  void route(int c){ model.sucessor(states.get(c));}
     
-    private void initializeHeuristic() {
-    	this.h.put("Arad", 366);
-    	this.h.put("Bucharest", 0);
-    	this.h.put("Craiova", 160);
-    	this.h.put("Drobeta", 242);
-    	this.h.put("Efoire", 161);
-    	this.h.put("Fagaras", 176);
-    	this.h.put("Giurgiu", 77);
-    	this.h.put("Hirsova", 151);
-    	this.h.put("Iasi", 226);
-    	this.h.put("Lugoj", 244);
-    	this.h.put("Mehadia", 241);
-    	this.h.put("Neamt", 234);
-    	this.h.put("Oradea", 380);
-    	this.h.put("Pitesti", 100);
-    	this.h.put("Rimnicu Vilcea", 193);
-    	this.h.put("Sibiu", 253);
-    	this.h.put("Timisoara", 329);
-    	this.h.put("Urziceni", 80);
-    	this.h.put("Vaslui", 199);
-    	this.h.put("Zerind", 374);
-    }
 }
